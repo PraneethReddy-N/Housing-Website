782476c1672bf0da8e6febf704a974b01e23793e
praneeth reddy <48400471+PraneethReddy-N@users.noreply.github.com>
2023-11-21 00:38:40 -0500

    Welcome Message once user logs into his account

--- routes/dashboards.js
+++ routes/dashboards.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res) =>{
-    res.render('dashboard');
+    let message = '';
+    if(req.session.message){
+        console.log(message);
+        message = req.session.message;
+        req.session.message = null;
+    }
+    res.render('dashboard',{message});
 })
 
 module.exports = router;
